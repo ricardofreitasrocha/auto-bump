50f14c2c0a331b09043c9d2bf196b40ecc7c2f70
ricardofreitasrocha <140706972+ricardofreitasrocha@users.noreply.github.com>
2025-07-16 16:59:39 +0100

    chore: version bump

--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: ["develop"],
+  branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/github",
