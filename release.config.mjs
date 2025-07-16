50cc306bbd1c584cd38f23a77a0f79f83a8ce5af
ricardofreitasrocha <140706972+ricardofreitasrocha@users.noreply.github.com>
2025-07-16 17:21:39 +0100

    chore(ruleset): change rules for releases

--- release.config.mjs
+++ release.config.mjs
@@ -4,7 +4,17 @@
 export default {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "patch", release: "patch" },
+        ],
+      }
+    ],
     "@semantic-release/github",
   ],
 };
